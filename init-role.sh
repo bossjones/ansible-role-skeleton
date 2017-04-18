e7a9fac4e1cfa12d11e011157aa37143df4d7c26
shadow <shadow@systemli.org>
2017-04-18 18:14:13 +0200

    Switched testing suite to tox and molecule
    * switching from vagrant testing to molecule, tox and docker
    * updated init/update-role.sh
    * added new .gitignore

--- init-role.sh
+++ init-role.sh
@@ -13,4 +13,4 @@ fi
 mkdir $ROLE_DIR
 cp -r $SKELETON_DIR $ROLE_DIR
 rm -rf $ROLE_DIR/.git $ROLE_DIR/.vagrant $ROLE_DIR/init-role.sh $ROLE_DIR/update-role.sh
-perl -p -i -e "s/$SKELETON_NAME/$ROLE_NAME/" $ROLE_DIR/README.md $ROLE_DIR/vagrant.yml $ROLE_DIR/tests/test.yml
+perl -p -i -e "s/$SKELETON_NAME/$ROLE_NAME/" $ROLE_DIR/README.md $ROLE_DIR/molecule.yml $ROLE_DIR/tests/test.yml
--- update-role.sh
+++ update-role.sh
@@ -10,9 +10,15 @@ if [ ! -d "$ROLE_DIR" ]; then
   exit 1
 fi
 
+rm $ROLE_DIR/vagrant.yml
+rm $ROLE_DIR/Vagrantfile
+rm $ROLE_DIR/tests/inventory
+
 cp -r $SKELETON_DIR/.travis.yml $ROLE_DIR
-cp -r $SKELETON_DIR/vagrant.yml $ROLE_DIR
-cp -r $SKELETON_DIR/Vagrantfile $ROLE_DIR
+cp -r $SKELETON_DIR/molecule.yml $ROLE_DIR
+cp -r $SKELETON_DIR/tox.ini $ROLE_DIR
 cp -r $SKELETON_DIR/tests/* $ROLE_DIR/tests
 
-perl -p -i -e "s/$SKELETON_NAME/$ROLE_NAME/" $ROLE_DIR/README.md $ROLE_DIR/vagrant.yml $ROLE_DIR/tests/test.yml
+perl -p -i -e "s/$SKELETON_NAME/$ROLE_NAME/" $ROLE_DIR/README.md $ROLE_DIR/molecule.yml $ROLE_DIR/tests/test.yml
+
+echo -n "If you use dependencies, such as ansible galaxy, uncomment the dependency lines in molecule.yml and add the dependencies in tests/requirements.yml\n"
