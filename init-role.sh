65e50d54321c0abb62aafcb5baee89419d24c9e6
t2d <td@tem.li>
2017-04-19 07:59:09 +0200

    Fix init-role.sh

--- init-role.sh
+++ init-role.sh
@@ -11,6 +11,6 @@ if [ -d "$ROLE_DIR" ]; then
 fi
 
 mkdir $ROLE_DIR
-cp -r $SKELETON_DIR $ROLE_DIR
+cp -r $SKELETON_DIR/* $ROLE_DIR
 rm -rf $ROLE_DIR/.git $ROLE_DIR/.vagrant $ROLE_DIR/init-role.sh $ROLE_DIR/update-role.sh
 perl -p -i -e "s/$SKELETON_NAME/$ROLE_NAME/" $ROLE_DIR/README.md $ROLE_DIR/molecule.yml $ROLE_DIR/tests/test.yml
