a6ffdedbea02be6e9ae1e4aea611a3d181a13f06
t2d <td@tem.li>
2017-04-19 07:51:25 +0200

    Check file presence before handling them

--- update-role.sh
+++ update-role.sh
@@ -5,19 +5,25 @@ ROLE_NAME=$(basename $ROLE_DIR)
 SKELETON_DIR=$(dirname $0)
 SKELETON_NAME="ansible-role-skeleton"
 
+absent="vagrant.yml Vagrantfile tests/inventory"
+present=".travis.yml molecule.yml tox.ini tests"
+
 if [ ! -d "$ROLE_DIR" ]; then
   echo "The directory $ROLE_DIR must exist!"
   exit 1
 fi
 
-rm $ROLE_DIR/vagrant.yml
-rm $ROLE_DIR/Vagrantfile
-rm $ROLE_DIR/tests/inventory
+for file in $absent; do
+  if [ -e ${ROLE_DIR}/${file} ]; then
+    rm ${ROLE_DIR}/${file}
+  fi
+done
 
-cp -r $SKELETON_DIR/.travis.yml $ROLE_DIR
-cp -r $SKELETON_DIR/molecule.yml $ROLE_DIR
-cp -r $SKELETON_DIR/tox.ini $ROLE_DIR
-cp -r $SKELETON_DIR/tests/* $ROLE_DIR/tests
+for file in $present; do
+  if [ ! -e ${ROLE_DIR}/${file} ]; then
+    cp -r ${SKELETON_DIR}/${file} ${ROLE_DIR}/
+  fi
+done
 
 perl -p -i -e "s/$SKELETON_NAME/$ROLE_NAME/" $ROLE_DIR/README.md $ROLE_DIR/molecule.yml $ROLE_DIR/tests/test.yml
 
